3ff91f2cdb6e11657426868031897e60ee1141b2
riltonbispo <riltondev@gmail.com>
2024-01-16 10:46:09 -0300

    refactor: form schema

--- src/app/schema/formSchema.ts
+++ src/app/schema/formSchema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
 export const loginSchema = z.object({
-  email: z.string().email().min(1, { message: 'Campo obrigatorio' }),
+  email: z.string().email({ message: 'Email Invalido' }),
   password: z.string().min(1, { message: 'Campo obrigatorio' }),
 })
