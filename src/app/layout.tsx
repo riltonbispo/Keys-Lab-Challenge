09dc234e1960766a0888d689bfd698db06d720e3
riltonbispo <riltondev@gmail.com>
2024-01-16 17:08:40 -0300

    feat: add default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import keylabsLogo from '@/assets/logo.png'
+import persona from '@/assets/persona.png'
+import Image from 'next/image'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +18,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex flex-col md:flex-row">
+          <header className="flex items-center gap-2 absolute top-10 left-10">
+            <Image src={keylabsLogo} alt="logo fox" />
+            <span className="text-xl font-bold">Keylab</span>
+          </header>
+          <div className="bg-blue-600">
+            <Image
+              src={persona}
+              alt="persona"
+              className="max-h-80 object-cover md:min-h-screen flex-shrink"
+            />
+          </div>
+          <div className="flex-1 px-6">
+            <div className="max-w-96 my-16 mx-auto flex flex-col items-center">
+              {children}
+            </div>
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,13 @@
 import Image from 'next/image'
-import foxLogo from '@/app/assets/fox.png'
-import keylabsLogo from '@/app//assets/logo.png'
-import persona from '@/app/assets/persona.png'
-import Form from '@/app/components/Form'
+import foxLogo from '@/assets/fox.png'
+import LoginForm from '@/components/LoginForm'
 
 export default function Home() {
   return (
-    <main className="flex flex-col md:flex-row">
-      <header className="flex items-center gap-2 absolute top-10 left-10">
-        <Image src={keylabsLogo} alt="logo fox" />
-        <span className="text-xl font-bold">Keylab</span>
-      </header>
-      <div className="bg-blue-600">
-        <Image
-          src={persona}
-          alt="persona"
-          className="max-h-80 object-cover md:min-h-screen flex-shrink"
-        />
-      </div>
-      <div className="flex-1 px-6">
-        <div className="max-w-96 my-16 mx-auto flex flex-col items-center">
-          <h1 className="text-xl font-semibold">Entrar na conta</h1>
-          <Image src={foxLogo} height={120} width={120} alt="logo fox" />
-          <Form />
-        </div>
-      </div>
-    </main>
+    <>
+      <h1 className="text-xl font-semibold">Entrar na conta</h1>
+      <Image src={foxLogo} height={120} width={120} alt="logo fox" />
+      <LoginForm />
+    </>
   )
 }
